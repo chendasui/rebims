0515196b61157ef1e4ab5e6f87a1be461cfb6eed
sunyard <das.chen@sunyard>
2014-01-19 13:07:35 +0800

    build the project

---
+++ src/main/java/com/dao/IUserDao.java
@@ -0,0 +1,14 @@
+package com.dao;
+
+import java.io.Serializable;
+
+import com.model.Tuser;
+
+/**
+ * @author 陈大碎
+ *
+ */
+public interface IUserDao {
+
+	public Serializable save(Tuser t );
+}
---
+++ src/main/java/com/service/IUserService.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.service;
+
+import java.io.Serializable;
+
+import com.model.Tuser;
+
+/**
+ * @author 陈大碎
+ *
+ */
+public interface IUserService {
+
+	public void test();
+	
+	public Serializable save(Tuser t);
+	
+}
